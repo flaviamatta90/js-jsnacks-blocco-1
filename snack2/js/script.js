d205119c42d97a9d219ec6c83b082f65a61f4799
Flavia <flaviamatta90@gmail.com>
2020-08-05 16:47:29 +0200

    secondo esercizio

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,1 +1,14 @@
+// 1 L'utente inserisce la prima parola
+var primaParola = prompt('Inserisci la prima parola');
 
+// 2 L'utente inserisce la seconda parola
+var secondaParola = prompt('Inserisci la seconda parola');
+
+// 3 il softwere stampa la parola più corta poi la parola più lunga
+if (primaParola.length < secondaParola.length) {
+  console.log(primaParola + " " + secondaParola);
+} else if (secondaParola.length < primaParola.length){
+  console.log(secondaParola + " " + primaParola);
+} else {
+  console.log('Le parole sono uguali');
+}
