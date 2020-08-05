8a5f99865fc50080fd0c05c89b72a788d62f71c7
Flavia <flaviamatta90@gmail.com>
2020-08-05 16:56:00 +0200

    terzo esercizio

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,1 +1,8 @@
-
+// 1 Inserisci un numero
+var numero = parseInt(prompt('Inserisci il numero'));
+// 2 Se è pari stampa il numero e se è dispari stampa il numero successivo
+if (numero % 2 == 0) {
+  console.log(numero);
+} else {
+  console.log(numero + 1);
+}
